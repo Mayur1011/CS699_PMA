3c94689526587d7353aa1142f9509a6f691d41fa
Mayur1011 <mayurpokharkar6@gmail.com>
2024-11-15 19:47:38 +0530

    Changed some content in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Transition } from "@headlessui/react";
-import clsx from "clsx";
 import { Fragment, useRef } from "react";
 import { IoClose } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
+import clsx from "clsx";
+
+// Redux
+import { setOpenSidebar } from "./redux/slices/authSlice";
+
+// Components
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+
+// Pages
 import Login from "./pages/Login";
 import TaskDetails from "./pages/TaskDetails";
 import Tasks from "./pages/Tasks";
 import Trash from "./pages/Trash";
 import Users from "./pages/Users";
 import Dashboard from "./pages/dashboard";
-import { setOpenSidebar } from "./redux/slices/authSlice";
 
 function Layout() {
   const { user } = useSelector((state) => state.auth);
@@ -100,13 +106,16 @@ function App() {
         <Route element={<Layout />}>
           <Route index path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/dashboard" element={<Dashboard />} />
+
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/completed/:status" element={<Tasks />} />
           <Route path="/in-progress/:status" element={<Tasks />} />
           <Route path="/todo/:status" element={<Tasks />} />
-          <Route path="/team" element={<Users />} />
-          <Route path="/trashed" element={<Trash />} />
           <Route path="/task/:id" element={<TaskDetails />} />
+
+          <Route path="/team" element={<Users />} />
+
+          <Route path="/trashed" element={<Trash />} />
         </Route>
 
         <Route path="/log-in" element={<Login />} />
