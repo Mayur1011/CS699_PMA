d73e0e68f329c069c90d313f32baff9fc43ce19b
Mayur1011 <mayurpokharkar6@gmail.com>
2024-11-17 00:16:41 +0530

    added

--- server/index.js
+++ server/index.js
@@ -18,10 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://6738e64e568f31d19d8bd588--snazzy-taffy-afa43d.netlify.app",
-    ],
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
